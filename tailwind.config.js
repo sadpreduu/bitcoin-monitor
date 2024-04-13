632a5d922a121587a73180e132b1131091e7303b
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-13 14:24:45 -0300

    feat: tailwind config done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = {
+  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
 
