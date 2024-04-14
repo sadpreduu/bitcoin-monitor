238ba96d65f575c1469a42d181980cf5ee04ee49
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-14 20:07:41 -0300

    feat: created a card wrapper to contain the price tracker also separeted the past 24 hour to be it own card

--- src/components/BitcoinTracker.vue
+++ src/components/BitcoinTracker.vue
@@ -1,39 +1,66 @@
-
 <template>
-    <section class="card-wrapper flex flex-col items-center justify-center">
-        <div class="card-content mb-4 text-4xl sm:text-5xl lg:text-6xl px-4 py-4">
-            <div>
-                <img class="inline-block h-10 w-fit sm:w-fit rounded-md container mx-auto self-center align-middle" src="/icons8-bitcoin.svg" alt="bitcoin logo">
-                <h1
-                    class="font-extrabold content-flex font-raleway align-middle px-2 w-full sm:w-fit bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-yellow-400 to-yellow-600 inline-block">
-                    Current Bitcoin <a class="underline decoration-4">Price:</a>
-            </h1>
-            </div>
+  <section class="flex flex-col items-center justify-center">
+    <div class="mb-4 text-4xl sm:text-5xl lg:text-6xl px-4 py-4 bg-local bg-gradient-to-r from-violet-500 via-violet-400 to-violet-600 rounded-md p-4">
+      <div class="flex">
+      
+        <h1
+          class="font-extrabold content-flex font-raleway align-middle px-2 w-full">
+          Current Bitcoin <a class="underline decoration-4">Price:</a>
+        </h1>
+      </div>
 
-            <div class="shadow-2xl rounded-md self-center px-0.5 py-0.5 w-full sm:w-fit bg-gradient-to-br from-orange-800 via-white-500 to-yellow-600 text-white mt-3"> 
-            <h2 class="font-playfair block bg-[#292828] px-7 py-5 self-center text-justify inset-0">USD ${{ bitcoinPrice }} 
-              <h2>
-                Past 24 Hours
-              </h2>
-            </h2>
-            </div>
-        </div>
-    </section>
+      <div
+        class="shadow-2xl rounded-md self-center px-0.5 py-0.5 w-full bg-gradient-to-br from-violet-800 via-white-500 to-violet-600 text-white mt-3">
+        <img class="flex h-10 w-fit rounded-md container mx-auto self-center align-middle"
+          src="/icons8-bitcoin.svg" alt="bitcoin logo">
+        <h2 class="flex font-playfair bg-[#292828] px-7 py-5 w-full self-center text-center inset-0 justify-center items-center">
+          USD {{ formattedBitcoinPrice }}
+        </h2>
+      </div>
+
+      <div
+        class="shadow-2xl rounded-md self-center px-0.5 py-0.5 w-full bg-gradient-to-br from-violet-800 via-white-500 to-violet-600 text-white mt-3">
+        <h2 class="flex font-raleway font-light text-sm self-stretch container mx-auto">
+          Past 24 Hours
+          <span v-if="priceChangePercentage > 0" class="text-green-500 ml-1" style="border: 1px solid green;">↑</span>
+          <span v-else-if="priceChangePercentage < 0" class="text-red-500 ml-1" style="border: 1px solid red;">↓</span>
+          <span v-else class="text-gray-500 ml-1" style="border: 1px solid gray;">-</span>
+
+        </h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
-    name: 'BitcoinTracker',
-    data() {
+  name: 'BitcoinTracker',
+  data() {
     return {
-      bitcoinPrice: null
+      bitcoinPrice: null,
+      bitcoinPrice24hAgo: null
     };
+  },
+  computed: {
+    formattedBitcoinPrice() {
+      return this.bitcoinPrice.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    },
+    priceChangePercentage() {
+      if (this.bitcoinPrice && this.bitcoinPrice24hAgo) {
+        const change = ((this.bitcoinPrice - this.bitcoinPrice24hAgo) / this.bitcoinPrice24hAgo) * 100;
+        return change.toFixed(2);
+      } else {
+        return null;
+      }
+    }
   },
   methods: {
     async fetchBitcoinPrice() {
       try {
         const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+        this.bitcoinPrice24hAgo = this.bitcoinPrice;
         this.bitcoinPrice = response.data.bitcoin.usd;
       } catch (error) {
         console.error('Error fetching Bitcoin price:', error);
@@ -42,10 +69,11 @@ export default {
   },
   mounted() {
     this.fetchBitcoinPrice(); 
+    setInterval(this.fetchBitcoinPrice, 30000); // Atualizar a cada 30 segundos
   }
 }
-
 </script>
+
 
 <style>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header>
         <nav>
            <div class="flex flex-wrap items-centefr justify-between mx-auto p-8">                   
-            <img src="/icons8-cat-eyes-50.png" class="rounded-md h-35 w-35 px-0.1 py-0.1 sm:h-10 sm:w-10 bg-gradient-to-r from-yellow-500 via-yellow-400 to-yellow-600" alt="LOGO">
+            <img src="/icons8-cat-eyes-50.png" class="rounded-md h-35 w-35 px-0.1 py-0.1 sm:h-10 sm:w-10 bg-gradient-to-r from-violet-800 via-white-500 to-violet-600" alt="LOGO">
              <div class="menu hidden md:block md:w-auto" id="navbar"> 
                 <ul class="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                     <li><router-link to="/">Home</router-link></li>
