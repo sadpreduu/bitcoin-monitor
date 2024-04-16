6178299936176c5bfe91e85a92ca20cadd3e8b92
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-16 11:39:27 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <div>
+    <Header/>
+    <HelloWorld/>
+    <WelcomeHeader/>
+    <BitcoinTracker/>
+    <TrackerHistory/>
+    <GraphHistory/>
+  </div>
+</template>
+
+<script>
 import Header from './components/Header.vue';
+import HelloWorld from './components/HelloWorld.vue';
 import WelcomeHeader from './components/WelcomeHeader.vue';
 import BitcoinTracker from './components/BitcoinTracker.vue';
 import TrackerHistory from './components/TrackerHistory.vue';
 import GraphHistory from './components/GraphHistory.vue';
 
-
+export default {
+  components: {
+    Header,
+    HelloWorld,
+    WelcomeHeader,
+    BitcoinTracker,
+    TrackerHistory,
+    GraphHistory
+  }
+}
 </script>
 
-<template>
-  <div>
+<style scoped>
+/* Seus estilos aqui */
+</style>
 
-  </div>
-  <Header/>
-  <HelloWorld/>
-  <WelcomeHeader/>
-  <BitcoinTracker/>
-  <TrackerHistory/>
-  <GraphHistory/>
-</template>
 
 <style scoped>
 
